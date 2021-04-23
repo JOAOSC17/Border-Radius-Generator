5ee65fddce7b3c626c1687c5e8d1413a861893b8
João Costa <costav1402@gmail.com>
2021-04-23 11:09:03 -0300

    modificando js e css

--- script.js
+++ script.js
@@ -6,22 +6,18 @@ var bottomLeftSlider = form.querySelector("#bottomLeft");
 var output = form.querySelector("#output");
 var box = document.querySelector("example-box");
 
-// 
 function resetBoxBorders() {
   box.style.borderRadius = "0 0 0 0";
 }
-
 function resetSliders() {
   topLeftSlider.value = 0;
   topRightSlider.value = 0;
   bottomRightSlider.value = 0;
   bottomLeftSlider.value = 0;
 }
-
 function renderOutput() {
   output.textContent = `border-radius: ${topLeftSlider.value}% ${topRightSlider.value}% ${bottomRightSlider.value}% ${bottomLeftSlider.value}%;`; 
 }
-
 function updateInterface() {
   box.style.borderTopLeftRadius = topLeftSlider.value + "%";
   box.style.borderTopRightRadius = topRightSlider.value + "%";
@@ -29,26 +25,18 @@ function updateInterface() {
   box.style.borderBottomLeftRadius = bottomLeftSlider.value + "%";
   renderOutput();
 }
-
 function resetInterface() {
   resetBoxBorders();
   resetSliders();
   renderOutput();
 }
-
-
-
 form.addEventListener('input', function(event) {
   updateInterface()
 })
 
-
-/* Event delegation */
 document.addEventListener('click', function(event) {
-  
-  event.preventDefault();
-    
-  if ( event.target.matches('#reset') ) {
+event.preventDefault();
+  if (event.target.matches('#reset') ) {
     resetInterface();
   }
   
